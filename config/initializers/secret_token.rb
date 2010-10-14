ae565614f61df733b261ac5f3adf9f0f04dfe515
Max <msprior@gmail.com>
2010-10-14 02:02:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maxprior::Application.config.secret_token = '653ece41be9bb8a040bf0005e2052cdb5b975c57632bc056a19601e1508eaad8e7b1e7f59297c297d9e10c6192f7f6c0b8858deede4630aa801e5736af8024b6'
